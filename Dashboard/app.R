63f83db30648ebc4a578e133d78884b4c7dc8646
Carlos M Guío B <cm.guiob@gmail.com>
2021-08-05 16:58:06 -0500

    finished app

--- Dashboard/app.R
+++ Dashboard/app.R
@@ -1,28 +1,29 @@
+################################################################################
+# Shiny app para explorar datos de suelos del PMA-PECIG
+#
+# Author: Carlos Guio
+# Created: 2021-01-22 
+################################################################################
+
+
+#------------------------------ IMPORTAR LIBRERIAS
+
 library(tidyverse)
 library(wesanderson)
 library(showtext)
 library(ggsn)
 library(ggrepel)
 library(sf)
+library(shinyWidgets)
 
-#------------------------------ LOAD
-
-temp <- tempfile() #Crear objetos temporales
-tempd <- tempdir()
-
-url <- "https://github.com/cmguiob/TERRAE_PECIG_dashaboard/blob/main/Dashboard/Dashboard_rds.zip?raw=true"
-
-download.file(url,temp, mode="wb")
-unzip(temp, exdir=tempd) #Descomprimir
-
-files_names <- list.files(tempd, pattern = "*.rds") #Leer nombres de archivos
-files_paths <- paste(file.path(tempd), files_names[], sep = "\\") #Crear rutas
-
-UCS_centroids <- files_paths %>% .[str_detect(.,"centroids")] %>% read_rds()
-deptos_RAO <- files_paths %>% .[str_detect(.,"RAO")] %>% read_rds()
+#------------------------------ CARGAR DATOS
 
 
-#----------------------------- PREP
+UCS_centroids <- read_rds("data/UCS_centroids.rds")
+deptos_RAO <- read_rds("data/deptos_RAO.rds")
+
+
+#----------------------------- PREPARAR
 
 pal <- wes_palette("Zissou1", 100, type = "continuous")
 
@@ -40,12 +41,12 @@ theme_update(panel.grid = element_blank(),
                                       size = 16),
              axis.title = element_blank(),
              axis.ticks =  element_line(color = "#c3beb8", size = .7),
-             legend.title = element_text(size = 20, 
-                                         face = "bold", 
-                                         color = "grey20", 
+             legend.title = element_text(size = 22, 
+                                         #face = "bold", 
+                                         color = "#474747", 
                                          family = "roboto"),
              legend.text = element_text(size = 16, 
-                                        color = "#c3beb8", 
+                                        color = "#474747", 
                                         family = "robotoc",
                                         face = "bold"),
              legend.key.size = unit(0.7, "cm"),
@@ -59,27 +60,44 @@ ui <- fluidPage(
   tags$head(
     # Note the wrapping of the string in HTML()
     tags$style(HTML("
-      @import url('https://fonts.googleapis.com/css?family=Playfair+Display|Roboto');
+      @import url('https://fonts.googleapis.com/css?family=Playfair+Display|Roboto|Roboto+Condensed');
       h1 {
-        font-family: 'Playfair Display';
+          font-family: 'Roboto';
+          font-weight: bold;
+          font-size: 32pt;
+          height: 60px;
+          text-align:right;
+          margin-bottom:15px;
       }
       .selectize-input {
-        height: 14px;
-        font-size: 12pt;
-        font-family: 'Roboto';
-        padding-top: 5px;
-        color: #c3beb8;
+          height: 14px;
+          font-size: 12pt;
+          font-family: 'Roboto Condensed';
+          padding-top: 5px;
+          color: #c3beb8;
+      }
+      body, label, input, button, select { 
+          font-family: Roboto Condensed;
+          font-size: 10 pt;
+          color: #474747;
+      }
+      .well {
+          background-color: #F0EBE5;
+          border-width: 0pt;
+      }
+      hr {
+          border-top: 1px solid #000000;
       }
       .shiny-input-container {
         color: #474747;
       }"))
   ),
-  headerPanel(title = "Unidades Cartográficas de Suelos del PMA - PECIG"),
+  headerPanel(title = "Explora los suelos del PMA - PECIG"),
   br(),
   sidebarPanel(
     selectInput(inputId = "departamento",
-              label = shiny::HTML("<p> <span style ='font-family: Roboto; font-size: 14pt;'> Selecciona un departamento </span></p>"),
-              choices = c("Guaviare","Meta", "Vichada", "Caqueta")),
+                label = shiny::HTML("<p> <span style ='font-family: Roboto; font-size: 16pt;'> Selecciona un departamento </span></p>"),
+                choices = c("Guaviare","Meta", "Vichada", "Caqueta")),
     plotOutput(outputId = "mapa")
   ),
   mainPanel(
@@ -98,30 +116,30 @@ server <- function(input, output, session) {
               aes(fill = RAO),
               color = NA)+
       ggrepel::geom_label_repel( data = UCS_centroids %>% 
-                          filter(DEPARTAMENTO == input$departamento)%>%
-                          filter(UCS_F == input$UCS),
-                        aes(label = UCS_F, geometry = geometry),
-                        alpha = 0.7,
-                        col = "grey20",
-                        size = 4,
-                        force_pull  = 0.2,
-                        max.overlaps = Inf,
-                        direction = "both",
-                        box.padding = 0.5,
-                        stat = "sf_coordinates",
-                        family = "roboto",
-                        segment.size = 0.3,
-                        segment.square = FALSE,
-                        segment.curvature = -0.3,
-                        segment.angle = 30, 
-                        segment.ncp = 10,
-                        show.legend = FALSE) +
+                                   filter(DEPARTAMENTO == input$departamento)%>%
+                                   filter(UCS_F == input$UCS),
+                                 aes(label = UCS_F, geometry = geometry),
+                                 alpha = 0.7,
+                                 col = "grey20",
+                                 size = 4,
+                                 force_pull  = 0.2,
+                                 max.overlaps = Inf,
+                                 direction = "both",
+                                 box.padding = 0.5,
+                                 stat = "sf_coordinates",
+                                 family = "robotoc",
+                                 segment.size = 0.3,
+                                 segment.square = FALSE,
+                                 segment.curvature = -0.3,
+                                 segment.angle = 30, 
+                                 segment.ncp = 10,
+                                 show.legend = FALSE) +
       ggsn::scalebar(data = deptos_RAO %>% filter(DEPARTAMENTO == input$departamento), 
                      dist = 25, 
                      dist_unit = "km",
                      transform = TRUE,
                      st.dist = 0.06,
-                     st.color = "grey20",
+                     st.color = "#474747",
                      st.size = 4,
                      height=0.02,
                      box.color = NA,
@@ -156,21 +174,32 @@ server <- function(input, output, session) {
       geom_label_repel(data = . %>% filter(UCS_F == input$UCS), 
                        aes(label = input$UCS),
                        alpha = 0.7,
-                       col = "grey20",
-                       size = 4,
+                       col = "#474747",
+                       size = 6,
                        direction = "y",
                        nudge_y = 0,
                        nudge_x = 0.05,
                        box.padding = 1,
-                       family = "roboto",
+                       family = "robotoc",
                        segment.size = 0.3,
                        segment.square = FALSE,
                        show.legend = FALSE)+
       geom_hline(aes(yintercept = 0.5), 
-                 color = "#c3beb8", 
+                 color = "#474747", 
                  size = 0.5, 
                  linetype = "dashed")+
-      stat_summary(fun = mean, geom = "point", size = 4, shape = 18, color = "grey20")+
+      geom_curve(
+        aes(x = 1.2, y = 0.5, xend = 0.95, yend = 0.25),
+        arrow = arrow(length = unit(0.07, "inch")), size = 0.2,
+        color = "#474747", curvature = 0.3) +
+      annotate(
+        geom = "text", 
+        x = 0.9, y = 0.16, family = "robotoc", 
+        size = 6, 
+        color = "#474747", 
+        lineheight = .9,
+        label = "Incertidumbre 0.5")+
+      stat_summary(fun = mean, geom = "point", size = 4, shape = 18, color = "#474747")+
       coord_flip() +
       guides(color = "none",
              size = guide_legend(title = "Tipos de suelos que contiene la UCS",
@@ -183,20 +212,27 @@ server <- function(input, output, session) {
       scale_colour_gradientn(colours = pal) +
       labs(x = NULL, y = "Incertidumbre - Rao")+
       theme(legend.position = "top",
-            axis.text.y = element_text(size = 20, face = "bold"),
-            panel.border = element_rect(fill = NA, colour = "#c3beb8"))
-  }, height = 370)
+            axis.text.y = element_text(size = 20),
+            panel.border = element_rect(fill = NA, colour = "#c3beb8", size = 1))
+  }, height = 360)
   
   output$UCS_select <- renderUI({
-    radioButtons(inputId = "UCS", 
-                 label = shiny::HTML("<p> <span style ='font-family: Roboto; font-size: 14pt'> Unidades Cartográficas de Suelos </span></p>"),
-                 choices = deptos_RAO %>% 
-                        st_drop_geometry()%>%
-                        filter(DEPARTAMENTO == input$departamento) %>% 
-                        distinct(UCS_F, .keep_all = TRUE) %>%
-                        arrange(desc(RAO)) %>%
-                        pull(UCS_F),
-                 inline = TRUE)
+    prettyRadioButtons(
+      inputId = "UCS", 
+      label = shiny::HTML("<p> <span style ='font-family: Roboto; font-size: 16pt'> Unidades Cartográficas de Suelos (UCS) </span></p>"),
+      choices = deptos_RAO %>% 
+        st_drop_geometry()%>%
+        filter(DEPARTAMENTO == input$departamento) %>% 
+        distinct(UCS_F, .keep_all = TRUE) %>%
+        arrange(desc(RAO)) %>%
+        pull(UCS_F),
+      choiceNames = list(
+        tags$span(style = "color:red")),
+      inline = TRUE,
+      status = "warning",
+      fill = TRUE
+    )
+    
   })
   
 }
